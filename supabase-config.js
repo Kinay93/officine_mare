05ed4b4f6c0c3e80de0a64a2c2bdb1ecbee9d645
Kinay93 <80257861+Kinay93@users.noreply.github.com>
2026-02-22 12:51:42 +0100

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 // supabase-config.js
 // ⚠️ Usa solo ANON KEY nel client (mai service_role)
-window.SUPABASE_URL = "https://TUO-PROGETTO.supabase.co";
-window.SUPABASE_ANON_KEY = "LA_TUA_ANON_KEY";
+window.SUPABASE_URL = "https://csecrsswysoxhtqvnisl.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzZWNyc3N3eXNveGh0cXZuaXNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2MDQzNjcsImV4cCI6MjA4NzE4MDM2N30.GVQktB6PcDHq7YlDcBqIG8lDA9cgq48eFmlM7GzjWGU";
