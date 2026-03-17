f9494ab6408c39c12d538a178f8ea4c785f2d3e9
Kinay93 <80257861+Kinay93@users.noreply.github.com>
2026-03-17 14:22:58 +0100

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,10 @@
 // supabase-config.js
 // ⚠️ Usa solo ANON KEY nel client (mai service_role)
-window.SUPABASE_URL = "https://csecrsswysoxhtqvnisl.supabase.co";
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzZWNyc3N3eXNveGh0cXZuaXNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2MDQzNjcsImV4cCI6MjA4NzE4MDM2N30.GVQktB6PcDHq7YlDcBqIG8lDA9cgq48eFmlM7GzjWGU";
+//window.SUPABASE_URL = "https://csecrsswysoxhtqvnisl.supabase.co";
+//window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzZWNyc3N3eXNveGh0cXZuaXNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2MDQzNjcsImV4cCI6MjA4NzE4MDM2N30.GVQktB6PcDHq7YlDcBqIG8lDA9cgq48eFmlM7GzjWGU";
+
+
+const SUPABASE_URL = "https://csecrsswysoxhtqvnisl.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzZWNyc3N3eXNveGh0cXZuaXNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2MDQzNjcsImV4cCI6MjA4NzE4MDM2N30.GVQktB6PcDHq7YlDcBqIG8lDA9cgq48eFmlM7GzjWGU";
+
+export { SUPABASE_URL, SUPABASE_ANON_KEY };
